0714f4a6662a8fef1d68db2ec19442873c79b919
tlamen <bernardolobo3@gmail.com>
2022-09-01 10:38:35 -0300

    :test_tube: testes de categoria

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  describe 'factory' do
+  describe 'validating category' do
     context 'when using standard factory' do
       it { expect(build(:category)).to be_valid }
     end
+
+    context 'when name is nil' do
+      it { expect(build(:category, name: '').to be_invalid)}
+    end
+
+    context 'when description is nil' do
+      it { expect(build(:category, description: '')).to be_invalid }
+    end
+
+    context 'when name has already been taken' do
+      before do
+        create(:category)
+      end
+      it { expect(build(:category)).to be_invalid }
+    end
   end
+
+  describe
 end
