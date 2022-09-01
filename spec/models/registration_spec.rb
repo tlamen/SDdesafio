364ef50821a4c1f2afa830d8c097ff2878ad5fc9
tlamen <bernardolobo3@gmail.com>
2022-09-01 11:14:38 -0300

    :test_tube: registration tests

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Registration, type: :model do
-  describe 'factory' do
+  describe 'validating registration' do
+    before { create(:user, id: 0) }
+    before { create(:course, id: 0) }
+
     context 'when using standard factory' do
       it { expect(build(:registration)).to be_valid }
+    end
+
+    context 'when registration is not linked to user' do
+      it { expect(build(:registration, user_id: nil)).to be_invalid }
+    end
+
+    context 'when registration is not linked to course' do
+      it { expect(build(:registration, course_id: nil)).to be_invalid }
     end
   end
 end
