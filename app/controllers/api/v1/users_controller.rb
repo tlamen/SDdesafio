b4b1fc56f2c976a07f8733894db7e6502379e1ff
tlamen <bernardolobo3@gmail.com>
2022-09-04 19:39:18 -0300

    :sparkles: método de login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::UsersController < ApplicationController
+    def login
+        user = User.find_by!(email: params[:email])
+        if user.valid_password?(params[:password])
+            render json: user
+        else
+            head status :unauthorized
+        end
+    rescue StandardError => e
+        render json: { message: e.message }, status :not_found
+    end 
 end
