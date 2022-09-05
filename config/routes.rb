ed10db75dd99ae27b051f4c1867b9a91f08d2c95
tlamen <bernardolobo3@gmail.com>
2022-09-05 16:24:00 -0300

    :construction: authentication token

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@
 Rails.application.routes.draw do
   devise_for :users, skip: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace 'api' do
+    namespace 'v1' do
+      scope 'users' do
+        post '/login', to: 'users#index'
+      end
+    end
+  end
 end
